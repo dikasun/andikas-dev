3d0c59c811df2fc07f4dbbc2b744360666aa6f55
andikas <andpuji27@gmail.com>
2023-08-20 11:16:07 +0700

    Fix image loader

--- src/shared/config/loader.ts
+++ src/shared/config/loader.ts
@@ -4,7 +4,7 @@ interface ImageLoaderProps {
 
 export default function imageKitLoader({ src }: ImageLoaderProps) {
   if (src[0] === "/") src = src.slice(1);
-  let urlEndpoint = process.env.IMAGEKIT_URL || "";
+  let urlEndpoint = "https://ik.imagekit.io/4o6binhtw/andikas/";
   if (urlEndpoint[urlEndpoint.length - 1] === "/")
     urlEndpoint = urlEndpoint.substring(0, urlEndpoint.length - 1);
   return `${urlEndpoint}/${src}`;
